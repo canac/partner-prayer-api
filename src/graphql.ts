91a9869557bb40fe15344179687a5ba71d44b866
Caleb Cox <canac@users.noreply.github.com>
2021-01-14 17:53:42 -0600

    Implement GraphQL mutations

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,8 +1,9 @@
 import { ApolloServer, gql } from 'apollo-server';
 import { GraphQLDate } from 'graphql-iso-date';
-import { getLastCompletedDay } from './db/completedDays';
+import { getLastCompletedDay, setLastCompletedDay } from './db/completedDays';
 import { getPartners } from './db/partners';
-import { getSchedule } from './db/schedule';
+import { generateSchedule, getSchedule } from './db/schedule';
+import { setSkippedDayStatus } from './db/skippedDays';
 import { Partner, Schedule } from './db/types';
 
 // Construct the GraphQL schema
@@ -26,7 +27,21 @@ const typeDefs = gql`
     partners: [Partner]
     schedule(month: Date!): Schedule
   }
+
+  type Mutation {
+    completeDay(day: Date!): Date
+    skipDay(day: Date!, isSkipped: Boolean!): Schedule
+  }
 `;
+
+type CompleteDayMutationParams = {
+  day: Date;
+}
+
+type SkipDayMutationParams = {
+  day: Date;
+  isSkipped: boolean;
+}
 
 type ScheduleQueryParams = {
   month: Date;
@@ -35,6 +50,17 @@ type ScheduleQueryParams = {
 // Provide resolver functions for the schema fields
 const resolvers = {
   Date: GraphQLDate,
+  Mutation: {
+    async completeDay(_: any, { day }: CompleteDayMutationParams): Promise<Date> {
+      await setLastCompletedDay(day);
+      return day;
+    },
+
+    async skipDay(_: any, { day, isSkipped}: SkipDayMutationParams): Promise<Schedule> {
+      await setSkippedDayStatus(day, isSkipped);
+      return await generateSchedule(day);
+    }
+  },
   Query: {
     async lastCompletedDay(): Promise<Date> {
       return await getLastCompletedDay();
