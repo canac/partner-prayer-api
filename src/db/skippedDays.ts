62c5529a45c48c99939447e50bc36afe29278422
Caleb Cox <canac@users.noreply.github.com>
2021-01-27 09:47:56 -0600

    Skip weekends by default

--- src/db/skippedDays.ts
+++ src/db/skippedDays.ts
@@ -1,15 +1,30 @@
-import { startOfMonth } from '../date-fns-utc';
+import { range } from 'lodash';
+import { endOfMonth, startOfMonth } from '../date-fns-utc';
 import { getDb } from './db';
 import { SkippedDayModel } from './types';
 
 // Return an array of the day indexes in the month that are skipped
-export async function getSkippedDays(month: Date): Promise<number[]> {
+export async function getSkippedDays(dirtyMonth: Date): Promise<number[]> {
   const db = await getDb();
-  const skippedDayDocs = await db.collection<SkippedDayModel>('skippedDays').find({
-    month: startOfMonth(month),
-    isSkipped: true,
-  }, { projection: { dayId: 1 } }).toArray();
-  return skippedDayDocs.map((day) => day.dayId);
+  const month = startOfMonth(dirtyMonth);
+  const skippedDayDocs = await db.collection<SkippedDayModel>('skippedDays').find({ month }).toArray();
+
+  // Calculate the weekends in the month
+  const numDaysInMonth = endOfMonth(month).getUTCDate();
+  const firstDayOfMonth = month.getUTCDay();
+  const weekends = range(numDaysInMonth).filter((dayId) => [0, 6].includes((dayId + firstDayOfMonth) % 7));
+
+  // Weekends are skipped by default
+  const skippedDays: Set<number> = new Set(weekends);
+  skippedDayDocs.forEach((doc) => {
+    if (doc.isSkipped) {
+      skippedDays.add(doc.dayId);
+    } else {
+      skippedDays.delete(doc.dayId);
+    }
+  });
+
+  return [...skippedDays];
 }
 
 // Update the skipped status of the given date
