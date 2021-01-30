7b6c624416231b6406756be8b69b5d4e778fcde6
Caleb Cox <canac@users.noreply.github.com>
2021-01-29 18:38:18 -0600

    Determine skipped days from the schedule

--- src/db/models.ts
+++ src/db/models.ts
@@ -1,12 +1,2 @@
-import { ObjectId } from 'mongodb';
-
+export { ObjectId } from 'mongodb';
 export { PartnerModel, ScheduleDayModel, ScheduleModel } from '../generated/graphql';
-
-export { ObjectId };
-
-export interface SkippedDayModel {
-  _id: ObjectId;
-  month: Date;
-  dayId: number; // zero-based index of the day from the start of the month
-  isSkipped: boolean;
-}
--- src/db/skippedDays.ts
+++ src/db/skippedDays.ts
@@ -1,26 +1,26 @@
 import { range } from 'lodash';
 import { endOfMonth, startOfMonth } from '../date-fns-utc';
+import { ScheduleModel } from '../generated/graphql';
 import { getDb } from './db';
-import { SkippedDayModel } from './models';
 
 // Return an array of the day indexes in the month that are skipped
 export async function getSkippedDays(dirtyMonth: Date): Promise<number[]> {
   const db = await getDb();
   const month = startOfMonth(dirtyMonth);
-  const skippedDayDocs = await db.collection<SkippedDayModel>('skippedDays').find({ month }).toArray();
 
   // Calculate the weekends in the month
   const numDaysInMonth = endOfMonth(month).getUTCDate();
   const firstDayOfMonth = month.getUTCDay();
   const weekends = range(numDaysInMonth).filter((dayId) => [0, 6].includes((dayId + firstDayOfMonth) % 7));
 
-  // Weekends are skipped by default
-  const skippedDays: Set<number> = new Set(weekends);
-  skippedDayDocs.forEach((doc) => {
-    if (doc.isSkipped) {
-      skippedDays.add(doc.dayId);
+  const schedule = await db.collection<ScheduleModel>('schedule').findOne({ month });
+  const scheduleDays = schedule?.days ?? [];
+  const skippedDays: Set<number> = new Set(weekends); // weekends are skipped by default
+  scheduleDays.forEach((day, index) => {
+    if (day.isSkipped) {
+      skippedDays.add(index);
     } else {
-      skippedDays.delete(doc.dayId);
+      skippedDays.delete(index);
     }
   });
 
@@ -30,9 +30,8 @@ export async function getSkippedDays(dirtyMonth: Date): Promise<number[]> {
 // Update the skipped status of the given date
 export async function setSkippedDayStatus(month: Date, dayId: number, isSkipped: boolean): Promise<void> {
   const db = await getDb();
-  await db.collection<SkippedDayModel>('skippedDays').updateOne(
-    { month: startOfMonth(month), dayId },
-    { $set: { isSkipped } },
-    { upsert: true },
+  await db.collection<ScheduleModel>('schedule').updateOne(
+    { month: startOfMonth(month) },
+    { $set: { [`days.${dayId}.isSkipped`]: isSkipped } },
   );
 }
